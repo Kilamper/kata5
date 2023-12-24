08c9fc6b1c01593db269d95e18f543895786348c
Kilamper <kilamper@gmail.com>
2023-12-24 14:04:12 +0000

    Command Inteface Created

---
+++ src/main/java/software/ulpgc/kata5/Command.java
@@ -0,0 +1,12 @@
+package software.ulpgc.kata5;
+
+public interface Command {
+    Output execute(Input input);
+    interface Input {
+        String get(String parameter);
+    }
+    interface Output {
+        int responseCode();
+        int result();
+    }
+}
